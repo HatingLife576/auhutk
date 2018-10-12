6b065f8f14569808b3a3290709134cb498fb8c78
HatingLife576 <42309277+HatingLife576@users.noreply.github.com>
2018-10-11 18:04:34 -0600

    Created The Index DOT PHP file

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+  <head>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <title>AUHA URL SHORTENER</title>
+  </head>
+  <body>
+    <input type="text" name="url">
+    <input type="submit" value="Submit URL">
+    <script type="text/javascript">
+      $(document).ready(function(){
+        $('input[type="submit"]').click(function(e){
+          e.preventDefault();
+          alert($('input[type="submit"]').val());
+        });
+      });
+    </script>
+  </body>
+</html>
